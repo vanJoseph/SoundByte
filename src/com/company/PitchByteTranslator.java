6f1c53abcd34d15e0c184121c42aaca4a04621a9
vanJoseph <donovan.wilder@gmail.com>
2019-01-30 03:02:14 -0500

    Added Java Doc comments

--- src/com/company/PitchByteTranslator.java
+++ src/com/company/PitchByteTranslator.java
@@ -3,13 +3,26 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is a Utility class that helps with the translation of pitch to bytes and the translation of bytes to pitch.
+ *
+ * @author Donovan J. Wilder
+ */
 public class PitchByteTranslator {
     List<Pitch> pitchBuffer = new ArrayList<>();
 
+    /**
+     * Returns the pitch list of the translated bytes from the constructur.
+     * @return  list of bytes translated from the constructor arguments
+     */
     public List<Pitch> getPitchBuffer() {
         return pitchBuffer;
     }
 
+    /**
+     * Translate a series of bytes into a list of pitches. You can access it by invoking the getPitchBufferMethod.
+     * @param datagram The byte array to translate into a pitch List.
+     */
     public PitchByteTranslator(Byte[] datagram) {
         for (byte b : datagram) {
             Pitch[] pitches=translateByteToPitch(b);
@@ -18,6 +31,11 @@ public class PitchByteTranslator {
         }
     }
 
+    /**
+     * Translate a byte into 2 pitchex. It returns 2 pitches because the pitches represents in hexadecimal 4 bits of the byte.
+     * @param byteO The byte to be translated
+     * @return The byte as 2 hexadecimal based pitches.
+     */
     public Pitch[] translateByteToPitch(byte byteO) {
         Pitch[] pitches = new Pitch[2];
         byte byte0 = (byte) ((byteO >>> 4) & 15);
@@ -25,6 +43,11 @@ public class PitchByteTranslator {
         return new Pitch[]{translateHexToPitch(byte0), translateHexToPitch(byte1)};
     }
 
+    /**
+     * Translate the last for bytes of the byte and turn it into its pitch equivalent.
+     * @param byteO The byte to be translated (only its last for bits)
+     * @return The Pitch representation of the bytes last for bits.
+     */
     public Pitch translateHexToPitch(byte byteO) {
         switch (byteO) {
             case 0:
