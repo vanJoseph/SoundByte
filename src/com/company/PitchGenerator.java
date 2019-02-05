3fba711211de68e61843c80d0a8b423a04314a6b
vanJoseph <donovan.wilder@gmail.com>
2019-02-05 18:10:06 -0500

    Java doc a lot of the code.

--- src/com/company/PitchGenerator.java
+++ src/com/company/PitchGenerator.java
@@ -5,9 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 //https://www.thecodingforums.com/threads/frequency-tone-generator-in-java-tia-sal22.739012/
 
+/**
+ * This class is used to create sound byte buffers.
+ *
+ * @author Donovan J. Wilder
+ */
 public class PitchGenerator {
-    public static float SAMPLE_RATE = 8000f;
+    public static float SAMPLE_RATE = 44100f;
+//Todo add a way to change the sample rate
 
+    /**
+     * Creates a wave byte buffer.
+     *
+     * @param hz the frequency of the the pitch
+     * @param msecs the duration the pitch should last
+     * @param vol the volume at which the pitch should
+     * @return the sound byte buffer of the
+     */
     public byte[] createPitchWave(int hz, int msecs, double vol) {
 
         if (hz <= 0)
@@ -26,7 +40,7 @@ public class PitchGenerator {
             buf[i] = (byte) (Math.sin(angle) * 127.0 * vol); //modified
         }
 
-// shape the front and back 10ms of the wave form
+        // shape the front and back 10ms of the wave form
         for (int i = 0; i < SAMPLE_RATE / 100.0 && i < buf.length / 2; i++) {
             buf[i] = (byte) (buf[i] * i / (SAMPLE_RATE / 100.0));
             buf[buf.length - 1 - i] =
@@ -35,12 +49,28 @@ public class PitchGenerator {
         return buf;
     }
 
+    /**
+     * Create the sound wave bytes by entering the Pitch.
+     *
+     * @param pitch the pitch at which the byte buffer should be
+     * @param msecs the duration of the byte bufffer
+     * @param vol the volume at which the sound should be played.
+     * @return the sound byte buffer of the wave pitch
+     */
     public byte[] createPitchWave(Pitch pitch, int msecs, double vol) {
         int fequency = (int)pitch.getFrequency();
         return createPitchWave(fequency, msecs, vol);
 
     }
 
+    /**
+     * Create a sound byte buffer of a sequence of pitches.
+     *
+     * @param msecs the duration of all the pitches
+     * @param vol the volume it should be at
+     * @param pitches the array of pitches
+     * @return a sound byte buffer of the sequence of pitches.
+     */
     public byte[] createPitchSequence(int msecs, double vol, Pitch... pitches) {
 
         if (pitches.equals(null)) {
@@ -49,10 +79,17 @@ public class PitchGenerator {
         byte[] sequence=null;
         for (Pitch pitch : pitches) {
             byte[] newWave = createPitchWave(pitch, msecs, vol);
-            sequence = (sequence==null) ? newWave : concat(sequence, newWave);
+            sequence = (sequence==null) ? newWave : concatBuffers(sequence, newWave);
         }
         return sequence;
     }
+
+    /**
+     * Plays the sound buffer audio.
+     *
+     * @param soundwave the sound byte buffer to play
+     * @throws LineUnavailableException
+     */
     public void playSoundBuff(byte [] soundwave) throws LineUnavailableException {
         AudioFormat af = new AudioFormat(SAMPLE_RATE,8,1,true,false);
         SourceDataLine sdl = AudioSystem.getSourceDataLine(af);
@@ -63,7 +100,14 @@ public class PitchGenerator {
         sdl.close();
     }
 
-    private byte[] concat(byte[] wave1, byte[] wave2) {
+    /**
+     * Concatenates  two byte buffers together
+     *
+     * @param wave1 the first byte buffer
+     * @param wave2 the second byte buffer
+     * @return the 2 byte buffer concatenated
+     */
+    private byte[] concatBuffers(byte[] wave1, byte[] wave2) {
         byte[] c = new byte[wave1.length + wave2.length];
         System.arraycopy(wave1, 0, c, 0,wave1.length);
         System.arraycopy(wave2,0,c,wave1.length,wave2.length);
