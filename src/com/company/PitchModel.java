99e17756e2468fc801e60351528a304237f53c7c
vanJoseph <donovan.wilder@gmail.com>
2019-02-14 21:46:55 -0500

    init commit

---
+++ src/com/company/PitchModel.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class PitchModel {
+    private double timeStamp;
+    private float pitch;
+    private float probability ;
+
+    public PitchModel(double timeStamp, float pitch, float probability) {
+        this.timeStamp=timeStamp;
+        this.pitch=pitch;
+        this.probability=probability;
+    }
+
+    public double getTimeStamp() {
+        return timeStamp;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public float getProbability() {
+        return probability;
+    }
+
+    @Override
+    public String toString() {
+        return "Timestamp: "+getTimeStamp()+" Pitch: "+getPitch()+ "Probability: "+getProbability();
+    }
+}
